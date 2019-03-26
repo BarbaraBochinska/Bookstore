80b3a9a67c863e56644f0c9bae4e19785267ebab
Barbara Bochinska <bachna22@gmail.com>
2019-03-26 16:22:55 +0100

    Added methods for BrowseViewClass, dispaying products by category etc

--- src/main/java/com/codecool/bookstore/View/BrowseView.java
+++ src/main/java/com/codecool/bookstore/View/BrowseView.java
@@ -1,8 +1,25 @@
 package com.codecool.bookstore.View;
 
+import com.codecool.bookstore.Model.ItemCategory;
+
+import java.util.ArrayList;
+
 public class BrowseView {
 
-    public void dejtext() {
-        System.out.println("jestesmy w browse");
+    public void showBrowseText(ItemCategory category) {
+        System.out.println("Available " + category + "S:");
+    }
+
+    public void showItems(ArrayList items) {
+        System.out.println("No. | Title | Price | Stock");
+        for (int i = 0 ; i < items.size(); i++) {
+            System.out.println(Integer.toString(i) + items.get(i));
+        }
+    }
+
+    public void showOptions() {
+        System.out.println("Choose action:");
+        System.out.println("1. Display info about product");
+        System.out.println("2. Back to main menu");
     }
 }
