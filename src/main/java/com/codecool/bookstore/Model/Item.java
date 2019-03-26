f599133486204058e567123f5be4fc1c2e7d860f
Barbara Bochinska <bachna22@gmail.com>
2019-03-26 15:00:11 +0100

    Created Item class and its custom constructor, also getter for ItemCategory

--- src/main/java/com/codecool/bookstore/Model/Item.java
+++ src/main/java/com/codecool/bookstore/Model/Item.java
@@ -1,4 +1,24 @@
 package com.codecool.bookstore.Model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Item {
+
+    private String title;
+    private float price;
+    private int stock;
+    private ItemCategory category;
+    private Map<ItemCategory, Integer> infoByCategory = new HashMap<>();
+
+    public Item(String title, float price, int stock, ItemCategory category) {
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
+
+    public ItemCategory getCategory() {
+        return category;
+    }
 }
