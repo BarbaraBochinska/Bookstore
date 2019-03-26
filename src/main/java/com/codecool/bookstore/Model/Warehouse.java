b10329b0dd4bc47de88f4635f6fc36992fd9a973
Barbara Bochinska <bachna22@gmail.com>
2019-03-26 15:01:03 +0100

    Creted class warehouse for saving new items there, each item category has its own arraylist

--- src/main/java/com/codecool/bookstore/Model/Warehouse.java
+++ src/main/java/com/codecool/bookstore/Model/Warehouse.java
@@ -1,4 +1,24 @@
 package com.codecool.bookstore.Model;
 
+import java.util.ArrayList;
+
 public class Warehouse {
+
+    private ArrayList<Item> books = new ArrayList<>();
+    private ArrayList<Item> games = new ArrayList<>();
+    private ArrayList<Item> movies = new ArrayList<>();
+
+    public void saveItem(Item item) {
+        switch (item.getCategory()) {
+            case BOOK:
+                books.add(item);
+                break;
+            case GAME:
+                games.add(item);
+                break;
+            case MOVIE:
+                movies.add(item);
+                break;
+        }
+    }
 }
