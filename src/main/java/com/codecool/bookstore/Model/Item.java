4a6b13b13a580f7ae2a7e04f7c95ab762f573471
Barbara Bochinska <bachna22@gmail.com>
2019-03-26 17:36:16 +0100

    Created getters for items variables

--- src/main/java/com/codecool/bookstore/Model/Item.java
+++ src/main/java/com/codecool/bookstore/Model/Item.java
@@ -6,16 +6,34 @@ import java.util.Map;
 public class Item {
 
     private String title;
+    private String description;
     private float price;
     private int stock;
     private ItemCategory category;
     private Map<ItemCategory, Integer> infoByCategory = new HashMap<>();
 
-    public Item(String title, float price, int stock, ItemCategory category) {
+    public Item(String title, String description, float price, int stock, ItemCategory category) {
         this.title = title;
+        this.description = description;
         this.price = price;
         this.stock = stock;
         this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     ItemCategory getCategory() {
@@ -25,4 +43,5 @@ public class Item {
     public String toString() {
         return " | " + title + " | " + price + "$ | " + stock;
     }
+
 }
