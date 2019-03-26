9ce0ca59555d0842b9291faa6dd6e64875c89323
Barbara Bochinska <bachna22@gmail.com>
2019-03-26 17:37:45 +0100

    Created method in new class InfoView for displaying info about items

--- src/main/java/com/codecool/bookstore/View/InfoView.java
+++ src/main/java/com/codecool/bookstore/View/InfoView.java
@@ -1,4 +1,13 @@
 package com.codecool.bookstore.View;
 
+import com.codecool.bookstore.Model.Item;
+
 public class InfoView {
+
+    public void showInfo(Item item){
+        System.out.println("Title: " + item.getTitle());
+        System.out.println("Description: " + item.getDescription());
+        System.out.println("Stock: " + item.getStock());
+        System.out.println("Price: " + item.getPrice());
+    }
 }
